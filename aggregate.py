c050868bcb92797f17ced27c91b6e8383dd1f876
Soichi Hayashi <hayashis@iu.edu>
2018-08-14 20:58:02 +0000

    added count mean/std

--- aggregate.py
+++ aggregate.py
@@ -7,18 +7,29 @@ import numpy
 plots = []
 
 #load densities and compute std/mean
-densities = []
 with open('config.json') as config_f:
     config = json.load(config_f)
+
+    densities = []
+    counts = []
     for output_dir in config["outputs"]:
         print("loading", output_dir+"/density.csv")
-        density  = numpy.genfromtxt(output_dir+"/density.csv", delimiter=',')
+        density = numpy.genfromtxt(output_dir+"/density.csv", delimiter=',')
         densities.append(density)
+
+        print("loading", output_dir+"/counts.csv")
+        count = numpy.genfromtxt(output_dir+"/count.csv", delimiter=',')
+        counts.append(count)
 
     density_std = numpy.std(densities, axis=0)
     numpy.savetxt('density.std.csv', density_std)
     density_mean = numpy.mean(densities, axis=0)
     numpy.savetxt('density.mean.csv', density_mean)
+
+    count_std = numpy.std(counts, axis=0)
+    numpy.savetxt('count.std.csv', count_std)
+    count_mean = numpy.mean(counts, axis=0)
+    numpy.savetxt('count.mean.csv', count_mean)
 
 #generate heatmap from density std/mean
 plot = {}
@@ -29,18 +40,40 @@ plot["data"] = [{
     "colorscale": "Portland",
     "z": density_std.tolist(),
 }]
-plot["layout"] = {}
+#plot["layout"] = {}
 plots.append(plot)
 
 plot = {}
 plot["type"] = "plotly"
-plot["name"] = "density std"
+plot["name"] = "density mean"
 plot["data"] = [{
     "type": "heatmap",
     "colorscale": "Hot",
     "z": density_mean.tolist(),
 }]
-plot["layout"] = {}
+#plot["layout"] = {}
+plots.append(plot)
+
+plot = {}
+plot["type"] = "plotly"
+plot["name"] = "count std"
+plot["data"] = [{
+    "type": "heatmap",
+    "colorscale": "Portland",
+    "z": count_std.tolist(),
+}]
+#plot["layout"] = {}
+plots.append(plot)
+
+plot = {}
+plot["type"] = "plotly"
+plot["name"] = "count mean"
+plot["data"] = [{
+    "type": "heatmap",
+    "colorscale": "Hot",
+    "z": count_mean.tolist(),
+}]
+#plot["layout"] = {}
 plots.append(plot)
 
 #save product.json
